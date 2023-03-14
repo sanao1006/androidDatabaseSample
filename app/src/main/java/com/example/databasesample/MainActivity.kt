528b276e889d9ecb31ae5470492b7860be8e7557
sanao1006 <naosak1006@gmail.com>
2023-03-14 15:33:15 +0900

    db処理

--- app/src/main/java/com/example/databasesample/MainActivity.kt
+++ app/src/main/java/com/example/databasesample/MainActivity.kt
@@ -29,6 +29,20 @@ class MainActivity : AppCompatActivity() {
 
     fun onSaveButtonClick(view: View){
         val etNote = findViewById<EditText>(R.id.etNote)
+        val note = etNote.text.toString()
+        val db = _helper.writableDatabase
+        val sqlDelete = "DELETE FROM cocktailmemos WHERE _id = ?"
+        var stmt = db.compileStatement(sqlDelete)
+        stmt.bindLong(1,_cocktailId.toLong())
+        stmt.executeUpdateDelete()
+
+        val sqlInsert = "INSERT INTO cocktailmemos (_id, name, note) VALUES (?, ?, ?)"
+        stmt = db.compileStatement(sqlInsert)
+        stmt.bindLong(1,_cocktailId.toLong())
+        stmt.bindString(2,_cocktailName)
+        stmt.bindString(3,note)
+        stmt.executeInsert()
+
         etNote.setText("")
         val tvCocktailName = findViewById<TextView>(R.id.tvCocktailName)
         tvCocktailName.text = getString(R.string.tv_name)
