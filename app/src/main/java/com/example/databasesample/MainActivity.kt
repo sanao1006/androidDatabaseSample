7508ff29b77f894ba636666d2ddce5f69af58d43
sanao1006 <naosak1006@gmail.com>
2023-03-14 14:10:48 +0900

    a

--- app/src/main/java/com/example/databasesample/MainActivity.kt
+++ app/src/main/java/com/example/databasesample/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.databasesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var _cocktailId = -1
+    private var _cocktailName = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val lvCocktail = findViewById<ListView>(R.id.lvCocktail)
+        lvCocktail.onItemClickListener = ListenItemClickListener()
+    }
+
+    fun onSaveButtonClick(view: View){
+        val etNote = findViewById<EditText>(R.id.etNote)
+        etNote.setText("")
+        val tvCocktailName = findViewById<TextView>(R.id.tvCocktailName)
+        tvCocktailName.text = getString(R.string.tv_name)
+        val btnSave = findViewById<Button>(R.id.btnSave)
+        btnSave.isEnabled = false
     }
 }
