6a5529a21106b5129111792f468a3b0a8eb647c4
sanao1006 <naosak1006@gmail.com>
2023-03-14 15:08:53 +0900

    ヘルパーオブジェクトの作成

--- app/src/main/java/com/example/databasesample/MainActivity.kt
+++ app/src/main/java/com/example/databasesample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.databasesample
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,11 +13,18 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     private var _cocktailId = -1
     private var _cocktailName = ""
+
+    private val _helper = DatabaseHelper(this@MainActivity)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val lvCocktail = findViewById<ListView>(R.id.lvCocktail)
         lvCocktail.onItemClickListener = ListenItemClickListener()
+    }
+
+    override fun onDestroy() {
+        _helper.close()
+        super.onDestroy()
     }
 
     fun onSaveButtonClick(view: View){
